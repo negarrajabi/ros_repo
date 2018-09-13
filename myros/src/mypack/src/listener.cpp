259c154522c8c3716129b750456b2c1afcfaefe4
hannahfaghihi <faghihihannah@gmail.com>
2018-09-14 00:44:53 +0430

    listener finished

--- myros/src/mypack/src/listener.cpp
+++ myros/src/mypack/src/listener.cpp
@@ -6,20 +6,16 @@ geometry_msgs::Twist msg;
 
 void chatterCallback(const geometry_msgs::Twist::ConstPtr& CurPose)
 {
-
-
-
-//         ROS_INFO("I heard: [%s]", msg->data.c_str());
-//         ROS_INFO(CurPose->x);
            std_msgs::String msgs;
            std::stringstream ss;
-       	   ss << "hello world "<<CurPose->linear.x;<<"     "<<CurPose->linear.y;
+       	   ss << "hello world "<<CurPose->linear.x;<<"     "<<CurPose->angular.z;
            msgs.data = ss.str(); 
            ROS_INFO("%s", msgs.data.c_str());
 	   msg.linear.x = CurPose->linear.x;
-           msg.linear.y = CurPose->linear.y;
-//	   turtle_vel.publish(msg);
-		 	
+           msg.angular.z = CurPose->angular.z;
+
+
+return;		 	
 }
 
 
@@ -29,24 +25,18 @@ int main(int argc, char **argv)
         ros::init(argc, argv, "listener");
         ros::NodeHandle n;
         ros::Rate loop_rate(10);
-         ros::Publisher turtle_vel = n.advertise<geometry_msgs::Twist>("turtle2/	cmd_vel", 5);
+        ros::Publisher turtle_vel = n.advertise<geometry_msgs::Twist>("turtle2/	cmd_vel", 5);
         ros::Subscriber sub = n.subscribe("turtle1/cmd_vel", 5, chatterCallback)
 
 while (ros::ok())
         {
 
+	 turtle_vel.publish(msg);
+	 ros::spinOnce();
+         loop_rate.sleep();
+        }
 
-
- turtle_vel.publish(msg);
- ros::spinOnce();
-
-                loop_rate.sleep();
-
-
-
-}
-
-return 0;
+	return 0;
 }
 
 
