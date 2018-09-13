b28435f5014b80e38606e1e67aac7246b85bdd76
negarrajabi <negar.271999@gmail.com>
2018-09-13 20:04:38 +0430

    ros responsiblities

---
+++ myros/build/subpub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/negar/mytutorials/src/subpub
+# Build directory: /home/negar/mytutorials/build/subpub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
