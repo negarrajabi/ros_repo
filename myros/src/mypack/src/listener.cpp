4e3bc1640a935c8d0411e0b5af0066f6fd728b90
hannahfaghihi <faghihihannah@gmail.com>
2018-09-14 00:11:28 +0430

    edit1 listener

--- myros/src/mypack/src/listener.cpp
+++ myros/src/mypack/src/listener.cpp
@@ -1,9 +1,20 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-void chatterCallback(const std_msgs::String::ConstPtr& CurPose)
+#include "turtlesim/Pose.h"
+
+
+void chatterCallback(const turtlesim::Pose::ConstPtr& CurPose)
 {
-         ROS_INFO("I heard: [%s]", msg->data.c_str());
-         ROS_INFO(CurPose->x);
+
+
+//         ROS_INFO("I heard: [%s]", msg->data.c_str());
+//         ROS_INFO(CurPose->x);
+           std_msgs::String msgs;
+           std::stringstream ss;
+       	   ss << "hello world "<<CurPose->x;<<"     "<<CurPose->y;
+           msgs.data = ss.str(); 
+           ROS_INFO("%s", msgs.data.c_str());
+		 	
 }
 
 
@@ -13,6 +24,7 @@ int main(int argc, char **argv)
         ros::init(argc, argv, "listener");
         ros::NodeHandle n;
         ros::Rate loop_rate(10);
-        ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+        ros::Publisher turtle = n.advertise<turtlesim::Pose>("turtle2/pose", 5);
+        ros::Subscriber sub = n.subscribe("turtle/pose", 10, chatterCallback);
 
 }
